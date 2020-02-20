4d8de83edef839c3796bbae69d8179be5f1245e1
TheSteamingArtist <stephen.josiah.jacot@gmail.com>
2020-02-19 21:48:12 -0500

    MadeDistanceScriptForHook

---
+++ GrappleProject/Assets/Stephen_Assets/Stephen_Scripts/HookReturnScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookReturnScript : MonoBehaviour
+{
+    public GameObject hookobj;
+
+    public Rigidbody hookammoRb;
+
+    float dist;
+
+    public GameObject player;
+
+    public GameObject ammoTransobj;
+
+    void Start()
+    {
+       
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Vector3.Distance(player.transform.position, hookobj.transform.position);
+
+        if(dist >= 18)
+        {
+            hookobj.transform.position = ammoTransobj.transform.position;
+            hookobj.transform.parent = ammoTransobj.transform;
+            hookammoRb.isKinematic = true;
+
+        }
+
+
+
+    }
+
+    
+}
